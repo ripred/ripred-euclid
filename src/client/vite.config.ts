be446331479006a3557b8bfe3563f233b9f96ba0
trent.wyatt <trent@trentwyatt.com>
2026-03-10 20:17:33 -0500

    build(client): empty external dist output before bundling

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react(), tailwind()],
   build: {
+    emptyOutDir: true,
     outDir: '../../dist/client',
     sourcemap: true,
     rollupOptions: {
